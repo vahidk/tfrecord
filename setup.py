61366482ed8cc83143e2474441ad25dadf40078f
Vahid Kazemi <vkazemi@gmail.com>
2019-11-27 21:13:28 -0800

    Update version.

--- setup.py
+++ setup.py
@@ -3,7 +3,7 @@
 from distutils.core import setup
 
 setup(name="tfrecord",
-      version="1.3",
+      version="1.4",
       description="TFRecord reader.",
       author="Vahid Kazemi",
       author_email="vkazemi@gmail.com",
