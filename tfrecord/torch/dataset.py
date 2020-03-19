f487819999d4b6d06dfb1e675dabd3b791d7833e
Ayush Karnawat <ayush.karnawat97@gmail.com>
2020-03-18 23:49:39 -0400

    General cleanup (#27)
    * Update .gitignore
    
    * Update setup.py
    
    * Docstrings and other minor changes
    
    * Revert "Update .gitignore"
    
    This reverts commit 2d2417cd048e1082a54dcd3406ab80347831a2e1.
    
    * Use sys.exit() instead of exit()
    
    * Import modules instead of classes/objects/etc.
    
    * Revert changes to iterators_utils
    
    * Support for python 2.6+
    
    * Keep original order of params
    
    Co-authored-by: Vahid Kazemi <vkazemi@gmail.com>

--- tfrecord/torch/dataset.py
+++ tfrecord/torch/dataset.py
@@ -1,12 +1,41 @@
+"""Load tfrecord files into torch datasets."""
+
+import typing
 import numpy as np
+
 import torch.utils.data
+
 from tfrecord import reader
 from tfrecord import iterator_utils
 
 
 class TFRecordDataset(torch.utils.data.IterableDataset):
-    def __init__(self, data_path, index_path, description, shuffle_queue_size=False):
-        super().__init__()
+    """Parse (generic) TFRecords dataset into `IterableDataset` object,
+    which contain `np.ndarrays`s.
+
+    Params:
+    -------
+    data_path: str
+        The path to the tfrecords file.
+
+    description: dict of (str, str), optional, default=None
+        Dictionary of (key, value) pairs where keys are the name of the
+        features and values correspond to data type. The data type can
+        be "byte", "float" or "int".
+
+    index_path: str
+        The path to the index file.
+
+    shuffle_queue_size: int, optional, default=None
+        Length of buffer. Determines how many records are queued to
+        sample from.
+    """
+    def __init__(self,
+                 data_path: str,
+                 index_path: str,
+                 description: typing.Dict[str, str],
+                 shuffle_queue_size: typing.Optional[int] = None) -> None:
+        super(TFRecordDataset, self).__init__()
         self.data_path = data_path
         self.index_path = index_path
         self.description = description
@@ -27,8 +56,39 @@ class TFRecordDataset(torch.utils.data.IterableDataset):
 
 
 class MultiTFRecordDataset(torch.utils.data.IterableDataset):
-    def __init__(self, data_pattern, index_pattern, splits, description, shuffle_queue_size=None):
-        super().__init__()
+    """Parse multiple (generic) TFRecords datasets into an `IterableDataset`
+    object, which contain `np.ndarrays`s.
+
+    Params:
+    -------
+    data_pattern: str
+        Input data path pattern.
+
+    index_pattern: str
+        Input index path pattern.
+
+    splits: dict
+        Dictionary of (key, value) pairs, where the key is used to
+        construct the data and index path(s) and the value determines
+        the contribution of each split to the batch.
+
+    description: dict of str
+        Dictionary of (key, value) pairs where keys are the name of the
+        features and values correspond to data type. The data type can
+        be "byte", "float" or "int".
+
+    shuffle_queue_size: int, optional, default=None
+        Length of buffer. Determines how many records are queued to
+        sample from.
+    """
+
+    def __init__(self,
+                 data_pattern: str,
+                 index_pattern: str,
+                 splits: typing.Dict[str, float],
+                 description: typing.Dict[str, str],
+                 shuffle_queue_size: typing.Optional[int] = None) -> None:
+        super(MultiTFRecordDataset, self).__init__()
         self.data_pattern = data_pattern
         self.index_pattern = index_pattern
         self.splits = splits
--- tfrecord/writer.py
+++ tfrecord/writer.py
@@ -2,6 +2,7 @@
 
 import io
 import struct
+import typing
 
 import numpy as np
 try:
@@ -13,23 +14,29 @@ from tfrecord import example_pb2
 
 
 class TFRecordWriter:
-    def __init__(self, data_path):
-        """Opens a tfrecord file for writing.
+    """Opens a TFRecord file for writing.
 
-        Args:
-            data_path: Path to the tfrecord file.
-        """
+    Params:
+    -------
+    data_path: str
+        Path to the tfrecord file.
+    """
+
+    def __init__(self, data_path: str) -> None:
         self.file = io.open(data_path, "wb")
 
-    def close(self):
+    def close(self) -> None:
         """Close the tfrecord file."""
         self.file.close()
 
-    def write(self, datum):
-        """Write an example into tfrecord file. 
+    def write(self, datum: typing.Dict[str, typing.Tuple[typing.Any, str]]) -> None:
+        """Write an example into tfrecord file.
 
-        Args:
-            Datum is a dictionary of tuples of form (value, dtype). dtype can be "byte", "float" or "int".
+        Params:
+        -------
+        datum: dict
+            Dictionary of tuples of form (value, dtype). dtype can be
+            "byte", "float" or "int".
         """
         record = TFRecordWriter.serialize_tf_example(datum)
         length = len(record)
@@ -40,7 +47,8 @@ class TFRecordWriter:
         self.file.write(TFRecordWriter.masked_crc(record))
 
     @staticmethod
-    def masked_crc(data):
+    def masked_crc(data: bytes) -> bytes:
+        """CRC checksum."""
         mask = 0xa282ead8
         crc = crc32c.crc32(data)
         masked = ((crc >> 15) | (crc << 17)) + mask
@@ -49,23 +57,29 @@ class TFRecordWriter:
         return masked_bytes
 
     @staticmethod
-    def serialize_tf_example(datum):
-        """Serialize example into tfrecord.Example proto. 
+    def serialize_tf_example(datum: typing.Dict[str, typing.Tuple[typing.Any, str]]) -> bytes:
+        """Serialize example into tfrecord.Example proto.
 
-        Args:
-            Datum is a dictionary of tuples of form (value, dtype). dtype can be "byte", "float" or "int".
+        Params:
+        -------
+        datum: dict
+            Dictionary of tuples of form (value, dtype). dtype can be
+            "byte", "float" or "int".
+
         Returns:
+        --------
+        proto: bytes
             Serialized tfrecord.example to bytes.
         """
-        features = {}
-        for key, (value, dtype) in datum.items():
-            feature = {
-                "byte": lambda f: example_pb2.Feature(bytes_list=example_pb2.BytesList(value=[f])),
-                "float": lambda f: example_pb2.Feature(float_list=example_pb2.FloatList(value=f)),
-                "int": lambda f: example_pb2.Feature(int64_list=example_pb2.Int64List(value=f))
-            }[dtype](value)
-            features[key] = feature
+        serialize = {
+            "byte": lambda f: example_pb2.Feature(
+                bytes_list=example_pb2.BytesList(value=[f])),
+            "float": lambda f: example_pb2.Feature(
+                float_list=example_pb2.FloatList(value=f)),
+            "int": lambda f: example_pb2.Feature(
+                int64_list=example_pb2.Int64List(value=f))
+        }
 
-        example_proto = example_pb2.Example(
-            features=example_pb2.Features(feature=features))
+        features = {key: serialize[dtype](value) for key, (value, dtype) in datum.items()}
+        example_proto = example_pb2.Example(features=example_pb2.Features(feature=features))
         return example_proto.SerializeToString()
