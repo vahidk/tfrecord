ee4beae60ba4902e25127e45962e1295a50b62c0
Richard Higgins <richard@relh.net>
2024-08-06 16:26:05 -0400

    v0 of map-style dataset

--- tfrecord/torch/__init__.py
+++ tfrecord/torch/__init__.py
@@ -1,4 +1,5 @@
 from tfrecord.torch import dataset
 
 from tfrecord.torch.dataset import TFRecordDataset
+from tfrecord.torch.dataset import TFRecordMapDataset
 from tfrecord.torch.dataset import MultiTFRecordDataset
--- tfrecord/torch/dataset.py
+++ tfrecord/torch/dataset.py
@@ -6,6 +6,7 @@ import numpy as np
 import torch.utils.data
 
 from tfrecord import reader
+from tfrecord.reader import TFRecordIndexer 
 from tfrecord import iterator_utils
 
 
@@ -90,6 +91,83 @@ class TFRecordDataset(torch.utils.data.IterableDataset):
             it = map(self.transform, it)
         return it
 
+
+class TFRecordMapDataset(torch.utils.data.Dataset):
+    """Parse (generic) TFRecords dataset into `Dataset` object,
+    which contain `np.ndarrays`s. By default (when `sequence_description`
+    is None), it treats the TFRecords as containing `tf.Example`.
+    Otherwise, it assumes it is a `tf.SequenceExample`.
+
+    Params:
+    -------
+    data_path: str
+        The path to the tfrecords file.
+
+    index_path: str
+        The path to the index file. Currently used for map-style dataset.
+
+    description: list or dict of str, optional, default=None
+        List of keys or dict of (key, value) pairs to extract from each
+        record. The keys represent the name of the features and the
+        values ("byte", "float", or "int") correspond to the data type.
+        If dtypes are provided, then they are verified against the
+        inferred type for compatibility purposes. If None (default),
+        then all features contained in the file are extracted.
+
+    shuffle_queue_size: int, optional, default=None
+        Length of buffer. Determines how many records are queued to
+        sample from.
+
+    transform : a callable, default = None
+        A function that takes in the input `features` i.e the dict
+        provided in the description, transforms it and returns a
+        desirable output.
+
+    sequence_description: list or dict of str, optional, default=None
+        Similar to `description`, but refers to the sequence features
+        within a `SequenceExample`. When this field is `None`, then it
+        is assumed that an `Example` is being read otherwise, a
+        `SequenceExample` is read. If an empty list or dictionary is
+        passed, then all features contained in the file are extracted.
+        Currently ignored.
+
+    compression_type: str, optional, default=None
+        The type of compression used for the tfrecord. Choose either
+        'gzip' or None.
+
+    """
+
+    def __init__(self,
+                 data_path: str,
+                 index_path: typing.Union[str, None],
+                 description: typing.Union[typing.List[str], typing.Dict[str, str], None] = None,
+                 shuffle_queue_size: typing.Optional[int] = None,
+                 transform: typing.Callable[[dict], typing.Any] = None,
+                 sequence_description: typing.Union[typing.List[str], typing.Dict[str, str], None] = None,
+                 compression_type: typing.Optional[str] = None,
+                 ) -> None:
+        super(TFRecordMapDataset, self).__init__()
+        self.data_path = data_path
+        self.index_path = index_path
+        self.description = description
+        self.sequence_description = sequence_description
+        self.shuffle_queue_size = shuffle_queue_size
+        self.transform = transform or (lambda x: x)
+        self.compression_type = compression_type
+        self.index = np.loadtxt(index_path, dtype=np.int64)[:, 0]
+        self.indexer = TFRecordIndexer(data_path, index_path, description, compression_type)
+
+    def __getitem__(self, index):
+        worker_info = torch.utils.data.get_worker_info()
+        if worker_info is not None:
+            shard = worker_info.id, worker_info.num_workers
+            np.random.seed(worker_info.seed % np.iinfo(np.uint32).max)
+        else:
+            shard = None
+        return self.transform(self.indexer.fetch(index))
+
+    def __len__(self):
+        return len(self.index)
 
 class MultiTFRecordDataset(torch.utils.data.IterableDataset):
     """Parse multiple (generic) TFRecords datasets into an `IterableDataset`
